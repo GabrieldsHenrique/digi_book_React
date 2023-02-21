58ee3946e6f896090977679d31ac38e5a39753f5
Gabriel Henrique <gabrielsilva.stk@gmail.com>
2023-02-21 19:33:57 -0300

    Criando Header

---
+++ src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+
+interface IProps{
+    children: React.ReactNode
+}
+
+const Layout = ({children} : IProps) => {
+  return <>
+  <main className='h-full scroll-auto  bg-gradient-to-b from-[#0C3459] via-[#5D9DD9] to-[#12518B] min-h-screen px-14 fonte'>
+    <Header></Header>
+    
+    <Footer></Footer>
+
+    </main>
+    </>
+}
+
+export default Layout
